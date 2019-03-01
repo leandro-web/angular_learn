d11da7672ad08c9a67cad71fb6ad4dd39e4e8a84
LEANDRO\Leandro <leandro-web@hotmail.com>
2019-03-01 10:54:38 -0300

    Displaying a list

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Logan' },
+    { id: 12, name: 'Mística' },
+    { id: 13, name: 'Jean Grey' },
+    { id: 14, name: 'Magneto' },
+    { id: 15, name: 'Charles Xavier' },
+    { id: 16, name: 'Gambit' },
+    { id: 17, name: 'Vampira' },
+    { id: 18, name: 'Tempestade' },
+    { id: 19, name: 'Scott' },
+    { id: 20, name: 'Jubileu' },
+];
